4ea5ce4267f4be573925fe2143a50593698c85a7
Rochak Pandey <rochakpandey56@gmail.com>
2024-03-06 01:12:36 +0530

    ⚠️fix in nextauth parameter type issues ( added @ts-ignore for now)

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from 'next-auth/next';
 import { authOptions } from 'src/utils/auth';
-
+// @ts-ignore
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
